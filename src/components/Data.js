d0e49582e84e19b3d43e7f9a63133f935acba42f
Tsolmon Togtokhsaikhan <23LP9801@G301-CC0180.local>
2023-12-05 13:11:33 +0800

    first commit

---
+++ src/components/Data.js
@@ -0,0 +1,254 @@
+fetch("https://dev.to/api/articles?username=gereltuyamz")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+[
+  {
+    type_of: "article",
+    id: 1615102,
+    title: "When to use dangerouslySetInnerHTML in React?",
+    description:
+      "Introduction   Rendering HTML markup from rich text created in a WYSIWYG editor might be...",
+    readable_publish_date: "Sep 29",
+    slug: "when-to-use-dangerouslysetinnerhtml-in-react-3ol",
+    path: "/gereltuyamz/when-to-use-dangerouslysetinnerhtml-in-react-3ol",
+    url: "https://dev.to/gereltuyamz/when-to-use-dangerouslysetinnerhtml-in-react-3ol",
+    comments_count: 0,
+    public_reactions_count: 0,
+    collection_id: null,
+    published_timestamp: "2023-09-29T03:22:16Z",
+    positive_reactions_count: 0,
+    cover_image:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--mxsa0VKl--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/r65dvo7hg07josz81xyq.jpeg",
+    social_image:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--Wo2WswjH--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/r65dvo7hg07josz81xyq.jpeg",
+    canonical_url:
+      "https://dev.to/gereltuyamz/when-to-use-dangerouslysetinnerhtml-in-react-3ol",
+    created_at: "2023-09-29T03:22:17Z",
+    edited_at: null,
+    crossposted_at: null,
+    published_at: "2023-09-29T03:22:16Z",
+    last_comment_at: "2023-09-29T03:22:16Z",
+    reading_time_minutes: 2,
+    tag_list: ["webdev"],
+    tags: "webdev",
+    user: {
+      name: "Gereltuya Munkhzaya",
+      username: "gereltuyamz",
+      twitter_username: null,
+      github_username: "GereltuyaMz",
+      user_id: 913694,
+      website_url: null,
+      profile_image:
+        "https://res.cloudinary.com/practicaldev/image/fetch/s--FrqAvEcf--/c_fill,f_auto,fl_progressive,h_640,q_auto,w_640/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/913694/b8a65d18-e427-4a62-9489-ee2d292f95cb.jpeg",
+      profile_image_90:
+        "https://res.cloudinary.com/practicaldev/image/fetch/s--Epp7lNAA--/c_fill,f_auto,fl_progressive,h_90,q_auto,w_90/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/913694/b8a65d18-e427-4a62-9489-ee2d292f95cb.jpeg",
+    },
+  },
+  {
+    type_of: "article",
+    id: 1612788,
+    title: "Static Assets",
+    description:
+      "Traveling is an enriching experience that opens up new horizons, exposes us to different cultures,...",
+    readable_publish_date: "Sep 29",
+    slug: "static-assets-3i6c",
+    path: "/gereltuyamz/static-assets-3i6c",
+    url: "https://dev.to/gereltuyamz/static-assets-3i6c",
+    comments_count: 0,
+    public_reactions_count: 0,
+    collection_id: null,
+    published_timestamp: "2023-09-29T02:46:24Z",
+    positive_reactions_count: 0,
+    cover_image:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--GtrSEs5D--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/5plb80fliysbz3xlyxha.png",
+    social_image:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--6dyPu9ME--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/5plb80fliysbz3xlyxha.png",
+    canonical_url: "https://dev.to/gereltuyamz/static-assets-3i6c",
+    created_at: "2023-09-27T05:13:11Z",
+    edited_at: "2023-09-29T02:46:26Z",
+    crossposted_at: null,
+    published_at: "2023-09-29T02:46:24Z",
+    last_comment_at: "2023-09-29T02:46:24Z",
+    reading_time_minutes: 2,
+    tag_list: ["programming"],
+    tags: "programming",
+    user: {
+      name: "Gereltuya Munkhzaya",
+      username: "gereltuyamz",
+      twitter_username: null,
+      github_username: "GereltuyaMz",
+      user_id: 913694,
+      website_url: null,
+      profile_image:
+        "https://res.cloudinary.com/practicaldev/image/fetch/s--FrqAvEcf--/c_fill,f_auto,fl_progressive,h_640,q_auto,w_640/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/913694/b8a65d18-e427-4a62-9489-ee2d292f95cb.jpeg",
+      profile_image_90:
+        "https://res.cloudinary.com/practicaldev/image/fetch/s--Epp7lNAA--/c_fill,f_auto,fl_progressive,h_90,q_auto,w_90/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/913694/b8a65d18-e427-4a62-9489-ee2d292f95cb.jpeg",
+    },
+  },
+  {
+    type_of: "article",
+    id: 1612791,
+    title: "Analytics",
+    description:
+      "Next.js Speed Insights allows you to analyze and measure the performance of pages using different...",
+    readable_publish_date: "Sep 27",
+    slug: "analytics-24ao",
+    path: "/gereltuyamz/analytics-24ao",
+    url: "https://dev.to/gereltuyamz/analytics-24ao",
+    comments_count: 0,
+    public_reactions_count: 0,
+    collection_id: null,
+    published_timestamp: "2023-09-27T05:14:53Z",
+    positive_reactions_count: 0,
+    cover_image:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--73J5KaiD--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/mycmqerzde63q3vkqvyh.jpeg",
+    social_image:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--_F5w7qrW--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/mycmqerzde63q3vkqvyh.jpeg",
+    canonical_url: "https://dev.to/gereltuyamz/analytics-24ao",
+    created_at: "2023-09-27T05:14:54Z",
+    edited_at: "2023-09-29T02:06:05Z",
+    crossposted_at: null,
+    published_at: "2023-09-27T05:14:53Z",
+    last_comment_at: "2023-09-27T05:14:53Z",
+    reading_time_minutes: 1,
+    tag_list: ["webdev"],
+    tags: "webdev",
+    user: {
+      name: "Gereltuya Munkhzaya",
+      username: "gereltuyamz",
+      twitter_username: null,
+      github_username: "GereltuyaMz",
+      user_id: 913694,
+      website_url: null,
+      profile_image:
+        "https://res.cloudinary.com/practicaldev/image/fetch/s--FrqAvEcf--/c_fill,f_auto,fl_progressive,h_640,q_auto,w_640/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/913694/b8a65d18-e427-4a62-9489-ee2d292f95cb.jpeg",
+      profile_image_90:
+        "https://res.cloudinary.com/practicaldev/image/fetch/s--Epp7lNAA--/c_fill,f_auto,fl_progressive,h_90,q_auto,w_90/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/913694/b8a65d18-e427-4a62-9489-ee2d292f95cb.jpeg",
+    },
+  },
+  {
+    type_of: "article",
+    id: 1612790,
+    title: "Lazy Loading",
+    description:
+      "Lazy loading in Next.js helps improve the initial loading performance of an application by decreasing...",
+    readable_publish_date: "Sep 27",
+    slug: "lazy-loading-4c17",
+    path: "/gereltuyamz/lazy-loading-4c17",
+    url: "https://dev.to/gereltuyamz/lazy-loading-4c17",
+    comments_count: 0,
+    public_reactions_count: 0,
+    collection_id: null,
+    published_timestamp: "2023-09-27T05:14:03Z",
+    positive_reactions_count: 0,
+    cover_image:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--oop8oSRa--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/z9oa8ng0pk2lsrk3yzkr.jpeg",
+    social_image:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--RE7i0BQm--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/z9oa8ng0pk2lsrk3yzkr.jpeg",
+    canonical_url: "https://dev.to/gereltuyamz/lazy-loading-4c17",
+    created_at: "2023-09-27T05:14:03Z",
+    edited_at: "2023-09-29T02:06:35Z",
+    crossposted_at: null,
+    published_at: "2023-09-27T05:14:03Z",
+    last_comment_at: "2023-09-27T05:14:03Z",
+    reading_time_minutes: 1,
+    tag_list: ["webdev"],
+    tags: "webdev",
+    user: {
+      name: "Gereltuya Munkhzaya",
+      username: "gereltuyamz",
+      twitter_username: null,
+      github_username: "GereltuyaMz",
+      user_id: 913694,
+      website_url: null,
+      profile_image:
+        "https://res.cloudinary.com/practicaldev/image/fetch/s--FrqAvEcf--/c_fill,f_auto,fl_progressive,h_640,q_auto,w_640/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/913694/b8a65d18-e427-4a62-9489-ee2d292f95cb.jpeg",
+      profile_image_90:
+        "https://res.cloudinary.com/practicaldev/image/fetch/s--Epp7lNAA--/c_fill,f_auto,fl_progressive,h_90,q_auto,w_90/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/913694/b8a65d18-e427-4a62-9489-ee2d292f95cb.jpeg",
+    },
+  },
+  {
+    type_of: "article",
+    id: 1612787,
+    title: "Font Optimization",
+    description:
+      "Google Fonts   Automatically self-host any Google Font. Fonts are included in the deployment...",
+    readable_publish_date: "Sep 27",
+    slug: "font-optimization-3k3o",
+    path: "/gereltuyamz/font-optimization-3k3o",
+    url: "https://dev.to/gereltuyamz/font-optimization-3k3o",
+    comments_count: 1,
+    public_reactions_count: 1,
+    collection_id: null,
+    published_timestamp: "2023-09-27T05:12:19Z",
+    positive_reactions_count: 1,
+    cover_image:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--Q6YPLvDJ--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/kdjqic88f9u8gv5vg8yx.jpeg",
+    social_image:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--0aEKBNI2--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/kdjqic88f9u8gv5vg8yx.jpeg",
+    canonical_url: "https://dev.to/gereltuyamz/font-optimization-3k3o",
+    created_at: "2023-09-27T05:12:20Z",
+    edited_at: "2023-09-29T02:07:21Z",
+    crossposted_at: null,
+    published_at: "2023-09-27T05:12:19Z",
+    last_comment_at: "2023-09-27T05:45:21Z",
+    reading_time_minutes: 1,
+    tag_list: ["webdev"],
+    tags: "webdev",
+    user: {
+      name: "Gereltuya Munkhzaya",
+      username: "gereltuyamz",
+      twitter_username: null,
+      github_username: "GereltuyaMz",
+      user_id: 913694,
+      website_url: null,
+      profile_image:
+        "https://res.cloudinary.com/practicaldev/image/fetch/s--FrqAvEcf--/c_fill,f_auto,fl_progressive,h_640,q_auto,w_640/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/913694/b8a65d18-e427-4a62-9489-ee2d292f95cb.jpeg",
+      profile_image_90:
+        "https://res.cloudinary.com/practicaldev/image/fetch/s--Epp7lNAA--/c_fill,f_auto,fl_progressive,h_90,q_auto,w_90/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/913694/b8a65d18-e427-4a62-9489-ee2d292f95cb.jpeg",
+    },
+  },
+  {
+    type_of: "article",
+    id: 1612784,
+    title: "Image Optimization in Next js",
+    description:
+      "According to Web Almanac, images account for a huge portion of the typical website’s page weight and...",
+    readable_publish_date: "Sep 27",
+    slug: "image-optimization-in-next-js-1e0f",
+    path: "/gereltuyamz/image-optimization-in-next-js-1e0f",
+    url: "https://dev.to/gereltuyamz/image-optimization-in-next-js-1e0f",
+    comments_count: 1,
+    public_reactions_count: 0,
+    collection_id: null,
+    published_timestamp: "2023-09-27T05:09:10Z",
+    positive_reactions_count: 0,
+    cover_image:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--UmZwGGPD--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/dsih98zf95q0o43f7m5m.png",
+    social_image:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--pjfr3lmL--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/dsih98zf95q0o43f7m5m.png",
+    canonical_url:
+      "https://dev.to/gereltuyamz/image-optimization-in-next-js-1e0f",
+    created_at: "2023-09-27T05:09:10Z",
+    edited_at: "2023-09-27T05:10:56Z",
+    crossposted_at: null,
+    published_at: "2023-09-27T05:09:10Z",
+    last_comment_at: "2023-09-27T06:09:43Z",
+    reading_time_minutes: 2,
+    tag_list: ["nextjs"],
+    tags: "nextjs",
+    user: {
+      name: "Gereltuya Munkhzaya",
+      username: "gereltuyamz",
+      twitter_username: null,
+      github_username: "GereltuyaMz",
+      user_id: 913694,
+      website_url: null,
+      profile_image:
+        "https://res.cloudinary.com/practicaldev/image/fetch/s--FrqAvEcf--/c_fill,f_auto,fl_progressive,h_640,q_auto,w_640/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/913694/b8a65d18-e427-4a62-9489-ee2d292f95cb.jpeg",
+      profile_image_90:
+        "https://res.cloudinary.com/practicaldev/image/fetch/s--Epp7lNAA--/c_fill,f_auto,fl_progressive,h_90,q_auto,w_90/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/913694/b8a65d18-e427-4a62-9489-ee2d292f95cb.jpeg",
+    },
+  },
+];
---
+++ src/components/Nav.js
@@ -0,0 +1,29 @@
+"use client";
+import styles from "../app/nav_style.module.css";
+
+export const Nav = () => {
+  return (
+    <div className={styles.nav}>
+      <img src="./Logo.svg" />
+
+      <div className={styles.hide}>
+        <img src="menu-outline.svg" />
+      </div>
+      <div className={styles.mid_nav}>
+        <p className="text-base text-[#3B3C4A]">Home</p>
+        <p className="text-base text-[#3B3C4A]">Blog</p>
+        <p className="text-base text-[#3B3C4A]">Contact</p>
+      </div>
+      <div className={styles.search}>
+        <input id={styles.search} type="text" placeholder="Search" />
+        <img src="search-outline.svg" />
+
+        {/* <div className={styles.theme}>
+          <div className={styles.kk}>
+            <img src="./Frame 205.svg" />
+          </div>
+        </div> */}
+      </div>
+    </div>
+  );
+};
